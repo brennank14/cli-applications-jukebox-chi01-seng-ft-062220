4c58d1cfdb54f97d0fe5fca1afd79875a62fe5c0
Kathleen Brennan <brennank14@gmail.com>
2020-06-19 19:19:54 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -1,1 +1,64 @@
-# Add your code here
+songs = [
+  "Phoenix - 1901",
+  "Tokyo Police Club - Wait Up",
+  "Sufjan Stevens - Too Much",
+  "The Naked and the Famous - Young Blood",
+  "(Far From) Home - Tiga",
+  "The Cults - Abducted",
+  "Phoenix - Consolation Prizes",
+  "Harry Chapin - Cats in the Cradle",
+  "Amos Lee - Keep It Loose, Keep It Tight"
+]
+
+def help
+  puts "I accept the following commands:"
+  puts "- help : displays this help message"
+  puts "- list : displays a list of songs you can play"
+  puts "- play : lets you choose a song to play"
+  puts "- exit : exits this program"
+end
+
+
+def list(array)
+  array.each_with_index do |value, index|
+    puts "#{index + 1}: #{value}"
+  end
+end
+
+
+def play(array)
+  puts "Please enter a song name or number:"
+  pick = gets.strip
+  if (array.include?(pick)) or (pick.to_i - 1 == array.each_with_index.index)
+    puts "Playing #{pick}"
+  else
+    puts "Invalid input, please try again"
+  end
+end
+
+
+def exit_jukebox
+  puts "Goodbye"
+end
+
+def run(array)
+  puts "Please enter a command:"
+  command = gets.strip
+  while command == "help" or command == "list" or command == "play" do
+    if command == "help"
+      help
+
+    elsif command == "list"
+      list(array)
+
+    elsif command == "play"
+      play(array)
+
+    end
+    puts "Please enter a command:"
+    command = gets.strip
+  end
+  if command == "exit"
+    exit_jukebox
+  end
+end
